176e8c2fd039e9f88001e63b8685b1a68a6858df
Anson Jablinski <anson@foreflight.com>
2017-09-22 15:14:33 -0500

    Fix timestamps
    - See comment for explanation.

--- HackRice.playground/Sources/TrackPointReader.swift
+++ HackRice.playground/Sources/TrackPointReader.swift
@@ -14,6 +14,23 @@ public struct TrackPoint: Decodable {
     public let accuracy_vert: Int
     public let bank: Double
     public let pitch: Double
+    
+    init(trackPointWithBadTimestamp: TrackPoint) {
+        // JSON is using seconds since 1970, but the decoder is using 1/1/2001.
+        // Reverse this bad conversion to get the correct date.
+        self.timestamp = Date(timeIntervalSince1970: trackPointWithBadTimestamp.timestamp.timeIntervalSinceReferenceDate)
+        self.latitude = trackPointWithBadTimestamp.latitude
+        self.longitude = trackPointWithBadTimestamp.longitude
+        self.course = trackPointWithBadTimestamp.course
+        self.speed_kts = trackPointWithBadTimestamp.speed_kts
+        self.altitude_ft = trackPointWithBadTimestamp.altitude_ft
+        self.pressure_altitude_ft = trackPointWithBadTimestamp.pressure_altitude_ft
+        self.source = trackPointWithBadTimestamp.source
+        self.accuracy_horiz = trackPointWithBadTimestamp.accuracy_horiz
+        self.accuracy_vert = trackPointWithBadTimestamp.accuracy_vert
+        self.bank = trackPointWithBadTimestamp.bank
+        self.pitch = trackPointWithBadTimestamp.pitch
+    }
 }
 
 enum FlightState {
@@ -29,6 +46,9 @@ public func getTrackPoints() -> [TrackPoint] {
         if let decodedPoints = try? decoder.decode([TrackPoint].self, from: data) {
             trackPoints = decodedPoints
         }
+    }
+    trackPoints = trackPoints.map {
+        TrackPoint(trackPointWithBadTimestamp: $0)
     }
     return trackPoints
 }
