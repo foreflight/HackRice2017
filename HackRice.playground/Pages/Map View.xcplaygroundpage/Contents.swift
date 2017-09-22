d8ce03bfe8e1e13c149693470513f53c9544d627
Anson Jablinski <anson@foreflight.com>
2017-09-22 14:39:10 -0500

    Streamline Map View for demo purposes

--- HackRice.playground/Pages/Map View.xcplaygroundpage/Contents.swift
+++ HackRice.playground/Pages/Map View.xcplaygroundpage/Contents.swift
@@ -7,59 +7,43 @@ class MapViewController: UIViewController, MKMapViewDelegate {
     let mapView = MKMapView(frame: .zero)
     var polyline = MKPolyline()
     
-    lazy var trackPoints: [TrackPoint] = {
-        return getTrackPoints()
-    }()
-    
     override func loadView() {
         self.view = mapView
-
-        NSLayoutConstraint.activate(
-            [mapView.topAnchor.constraint(equalTo: self.view.topAnchor),
-             mapView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
-             mapView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
-             mapView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)]
-        )
-    }
-    
-    override func viewDidLoad() {
         mapView.delegate = self
-        displayTrackPoints()
-        super.viewDidLoad()
     }
     
     func displayTrackPoints() {
-        DispatchQueue.global(qos: .userInitiated).async {
-            _ = self.trackPoints //force lazy load
-            DispatchQueue.main.async {
-                self.addTrackPointOverlay()
-                self.mapView.visibleMapRect = self.polyline.boundingMapRect
-            }
+        // Load points from JSON
+        let points = getTrackPoints()
+        
+        // Transform points into a polyline for the map
+        let coords: [CLLocationCoordinate2D] = points.map {
+            CLLocationCoordinate2D(latitude: $0.latitude, longitude: $0.longitude)
         }
+        polyline = MKPolyline(coordinates: coords, count: coords.count)
+        
+        // Add polyline to map
+        mapView.add(polyline)
+
+        // Zoom map to fit the points
+        mapView.visibleMapRect = polyline.boundingMapRect
     }
     
-    func addTrackPointOverlay() {
-        polyline = generatePolyline(trackPoints: self.trackPoints)
-        self.mapView.add(polyline)
-    }
-    
+    // MKMapView delegate method
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
         let overlayRenderer = MKPolylineRenderer(polyline: polyline)
         overlayRenderer.strokeColor = .blue
         overlayRenderer.lineWidth = 3.0
         return overlayRenderer
     }
-    
 }
 
-func generatePolyline(trackPoints: [TrackPoint]) -> MKPolyline {
-    let coords: [CLLocationCoordinate2D] = trackPoints.map {
-        CLLocationCoordinate2D(latitude: $0.latitude, longitude: $0.longitude)
-    }
-    let polyline = MKPolyline(coordinates: coords, count: coords.count)
-    return polyline
-}
+// Create map view controller
+let mapViewController = MapViewController(nibName: nil, bundle: nil)
+// Tell Playground to display the view
+PlaygroundPage.current.liveView = mapViewController
 
-PlaygroundPage.current.liveView = MapViewController(nibName: nil, bundle: nil)
+// Tell map view controller to display the track points
+mapViewController.displayTrackPoints()
 
 //: [Next](@next)
